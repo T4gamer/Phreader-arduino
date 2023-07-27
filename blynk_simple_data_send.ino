3e4234dcb23ac7e87eeb5ce0cea9b325c82d5470
t4gamerz <tahagadban@gmail.com>
2023-07-27 22:27:37 +0300

    comm over serial http server

--- blynk_simple_data_send.ino
+++ blynk_simple_data_send.ino
@@ -1,72 +1,48 @@
-/* Fill-in information from Blynk Device Info here */
-#define BLYNK_TEMPLATE_ID           "TMPL25GU6SnIr"
-#define BLYNK_TEMPLATE_NAME         "PH Tripoli"
-#define BLYNK_AUTH_TOKEN            "cLolM734TLYcbjBCCh97oDB1SN9e4g1z"
+#include <ESP8266WiFi.h>
+#include <ESP8266WebServer.h>
 
-/* Comment this out to disable prints and save space */
-#define BLYNK_PRINT Serial
+const char* ssid = "ph";
+const char* password = "12345678";
 
+ESP8266WebServer server(80);
 
-#include <ESP8266_Lib.h>
-#include <BlynkSimpleShieldEsp8266.h>
-#include <SoftwareSerial.h>
-#include <LiquidCrystal_I2C.h> // Library for LCD
+typedef union {  float number;  
+  uint8_t bytes[4];  } FLOATUNION_t;
+FLOATUNION_t float1, float2; 
 
-// Your WiFi credentials.
-// Set password to "" for open networks.
-char ssid[] = ".......@......";
-char pass[] = "ASAS@cxz";
-
-// Hardware Serial on Mega, Leonardo, Micro...
-#define EspSerial Serial1
-LiquidCrystal_I2C lcd(0x27, 16, 2); // I2C address 0x27, 16 column and 2 rows
-
-// or Software Serial on Uno, Nano...
-SoftwareSerial EspSerial(2, 3); // RX, TX
-
-// Your ESP8266 baud rate:
-#define ESP8266_BAUD 115200
-
-ESP8266 wifi(&EspSerial);
-
-BlynkTimer timer;
-double ph = 0.0;
-
-// This function sends Arduino's up time every second to Virtual Pin (5).
-// In the app, Widget's reading frequency should be set to PUSH. This means
-// that you define how often to send data to Blynk App.
-void myTimerEvent()
-{
-  // You can send any value at any time.
-  // Please don't send more that 10 values per second.
-  lcd.setCursor(0, 0);
-  lcd.print("ph reading:");
-  lcd.setCursor(0, 1);
-  lcd.print(millis() / 1000);
-  Blynk.virtualWrite(V5, millis() / 1000);
+void clearSerialBuffer() {
+  while (Serial.available() > 0) {
+    Serial.read();
+  }
 }
 
-void setup()
-{
-  // Debug console
-  Serial.begin(4800);
-lcd.init(); //initialize the lcd
-  lcd.backlight(); //open the backlight
-  // Set ESP8266 baud rate
-  EspSerial.begin(ESP8266_BAUD);
-  delay(10);
 
-  Blynk.begin(BLYNK_AUTH_TOKEN, wifi, ssid, pass);
-  // You can also specify server:
-  //Blynk.begin(BLYNK_AUTH_TOKEN, wifi, ssid, pass, "blynk.cloud", 80);
-  //Blynk.begin(BLYNK_AUTH_TOKEN, wifi, ssid, pass, IPAddress(192,168,1,100), 8080);
-
-  // Setup a function to be called every second
-  timer.setInterval(1000L, myTimerEvent);
+void handleEndpoint1() {
+  clearSerialBuffer();
+  Serial.readBytes(float1.bytes, 4);
+  Serial.readBytes(float2.bytes, 4);
+  server.send(200, "text/plain", String(float1.number));
+}
+void handleEndpoint2() {
+  clearSerialBuffer();
+  Serial.readBytes(float1.bytes, 4);
+  Serial.readBytes(float2.bytes, 4);
+  server.send(200, "text/plain", String(float2.number));
 }
 
-void loop()
-{
-  Blynk.run();
-  timer.run(); // Initiates BlynkTimer
+  
+void setup() {
+  Serial.begin(9600);
+  WiFi.softAP(ssid, password);
+  IPAddress myIP = WiFi.softAPIP();
+  Serial.print("AP IP address: ");
+  Serial.println(myIP);
+  server.on("/value1", handleEndpoint1);
+  server.on("/value2", handleEndpoint2);
+
+  server.begin();
+}
+
+void loop() {
+  server.handleClient();
 }
--- readph.ino
+++ readph.ino
@@ -1,24 +1,31 @@
 #include <Wire.h>
 #include <LiquidCrystal_I2C.h>
+#include <OneWire.h>
+#include <DallasTemperature.h>
+
+#define ONE_WIRE_BUS 4
+OneWire oneWire(ONE_WIRE_BUS);
+DallasTemperature sensors(&oneWire);
 LiquidCrystal_I2C lcd(0x27, 16, 2);
+
 float calibration_value = 21.34;
 int phval = 0;
+float ph_act = 0;
 unsigned long int avgval;
-int buffer_arr[10], temp;
+int buffer_arr[10] ,temp;
 
-typedef union
-{
-  float number;
-  uint8_t bytes[4];
-} FLOATUNION_t;
-FLOATUNION_t phreading_to_send;
-FLOATUNION_t tempreading_to_send;
+typedef union {  float number;  
+  uint8_t bytes[4];  } FLOATUNION_t;;
+
 void setup()
 {
-  pinMode(A2,INPUT);
-  Serial.begin(115200);
+  sensors.begin();
+  
+  Serial.begin(9600);
+  
   lcd.init();
   lcd.begin(16, 2);
+  
   lcd.backlight();
   lcd.setCursor(0, 0);
   lcd.print("   Bienvenido a    ");
@@ -27,7 +34,13 @@ void setup()
   delay(2000);
   lcd.clear();
 }
-void loop() {
+void loop() {  
+  
+  FLOATUNION_t float1, float2; 
+  sensors.requestTemperatures();
+  float1.number = ph_act;
+  float2.number = sensors.getTempCByIndex(0);
+  
   for (int i = 0; i < 10; i++)
   {
     buffer_arr[i] = analogRead(A0);
@@ -49,14 +62,18 @@ void loop() {
   for (int i = 2; i < 8; i++)
     avgval += buffer_arr[i];
   float volt = (float)avgval * 5.0 / 1024 / 4.3;
-  float ph_act = -5.70 * volt +29.5 ;
+  ph_act = -5.70 * volt +29.5 ;
   lcd.setCursor(0, 0);
   lcd.print("pH Val:");
-  lcd.setCursor(6,1);
+  lcd.setCursor(8,0);
   lcd.print(ph_act);
-  phreading_to_send.number = ph_act;
-  tempreading_to_send.number = analogRead(A2);
-  Serial.write(phreading_to_send.bytes , 4);
-  Serial.write(tempreading_to_send.bytes, 4);
+  lcd.setCursor(0, 1);
+  lcd.print("Temp:");
+  lcd.setCursor(6,1);
+  lcd.print(float2.number);
+
+  Serial.write(float1.bytes, 4);  
+  Serial.write(float2.bytes, 4);
+  
   delay(500);
 }
