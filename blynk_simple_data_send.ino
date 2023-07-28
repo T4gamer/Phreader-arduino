c476123195624cbde041e2b434eeefcc93f8d68c
t4gamerz <tahagadban@gmail.com>
2023-07-28 03:02:40 +0300

    i think i fixed the error of the serial comm

--- blynk_simple_data_send.ino
+++ blynk_simple_data_send.ino
@@ -8,29 +8,52 @@ ESP8266WebServer server(80);
 
 typedef union {  float number;  
   uint8_t bytes[4];  } FLOATUNION_t;
-FLOATUNION_t float1, float2; 
+FLOATUNION_t float1, float2;
+
+const int bufferSize = 8;
+const int subsetSize = 4;
+byte buffer[bufferSize];
+int bufferIndex = 0;
+int startIndex = bufferSize - subsetSize;
 
 void clearSerialBuffer() {
-  while (Serial.available() > 0) {
+  while (Serial.available() > 8) {
     Serial.read();
   }
 }
 
-
 void handleEndpoint1() {
   clearSerialBuffer();
-  Serial.readBytes(float1.bytes, 4);
-  Serial.readBytes(float2.bytes, 4);
+  Serial.readBytes(buffer,8);
+//  if (Serial.available()) {
+//    byte incomingByte = Serial.read();
+//    buffer[bufferIndex] = incomingByte;
+//    bufferIndex++;
+//    if (bufferIndex == bufferSize) {
+//      bufferIndex = 0;
+//    }
+//  }
+  memcpy(float1.bytes, buffer, subsetSize);
   server.send(200, "text/plain", String(float1.number));
 }
 void handleEndpoint2() {
   clearSerialBuffer();
-  Serial.readBytes(float1.bytes, 4);
-  Serial.readBytes(float2.bytes, 4);
+  Serial.readBytes(buffer,8);
+//  if (Serial.available()) {
+//    byte incomingByte = Serial.read();
+//    buffer[bufferIndex] = incomingByte;
+//    bufferIndex++;
+//    if (bufferIndex == bufferSize) {
+//      bufferIndex = 0;
+//    }
+//  }
+  memcpy(float2.bytes, &buffer[startIndex], subsetSize);
   server.send(200, "text/plain", String(float2.number));
 }
 
-  
+void handleEndpoint3() {
+  ESP.restart();
+}
 void setup() {
   Serial.begin(9600);
   WiFi.softAP(ssid, password);
@@ -39,6 +62,7 @@ void setup() {
   Serial.println(myIP);
   server.on("/value1", handleEndpoint1);
   server.on("/value2", handleEndpoint2);
+  server.on("/rest", handleEndpoint3);
 
   server.begin();
 }
